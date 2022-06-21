e1cf4ca1d431a46d8d1b4ce87728afb283318aee
flopsif <florian.l.frauenfelder@gmail.com>
2022-06-21 11:06:09 +0200

    Optimize imports

--- app/src/main/java/me/frauenfelderflorian/tournamentscompose/ui/theme/Theme.kt
+++ app/src/main/java/me/frauenfelderflorian/tournamentscompose/ui/theme/Theme.kt
@@ -2,18 +2,16 @@ package me.frauenfelderflorian.tournamentscompose.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.view.Window
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
