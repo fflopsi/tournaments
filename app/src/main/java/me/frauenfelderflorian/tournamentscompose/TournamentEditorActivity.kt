ed096e754351aa0d845bbadefeea4fc0c7b64f72
flopsif <florian.l.frauenfelder@gmail.com>
2022-07-12 17:07:13 +0200

    Add start and end date pickers Remove testing code

--- app/src/main/java/me/frauenfelderflorian/tournamentscompose/TournamentEditorActivity.kt
+++ app/src/main/java/me/frauenfelderflorian/tournamentscompose/TournamentEditorActivity.kt
@@ -8,17 +8,13 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import kotlinx.coroutines.launch
 import me.frauenfelderflorian.tournamentscompose.data.Tournament
 import me.frauenfelderflorian.tournamentscompose.ui.theme.TournamentsComposeTheme
 import java.util.*
@@ -31,8 +27,10 @@ fun TournamentEditor(navController: NavController, tournaments: MutableList<Tour
     val scope = rememberCoroutineScope()
     val hostState = remember { SnackbarHostState() }
 
-    val name = rememberSaveable { mutableStateOf("") }
-    val date = rememberSaveable { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("") }
+    var start by rememberSaveable { mutableStateOf(GregorianCalendar()) }
+    var end by rememberSaveable { mutableStateOf(GregorianCalendar()) }
+    if (start.after(end)) end = start.clone() as GregorianCalendar
 
     TournamentsComposeTheme {
         Scaffold(
@@ -47,12 +45,10 @@ fun TournamentEditor(navController: NavController, tournaments: MutableList<Tour
                     },
                     actions = {
                         IconButton(onClick = {
-                            scope.launch {
-                                hostState.showSnackbar(
-                                    "test",
-                                    duration = SnackbarDuration.Short
-                                )
-                            }
+                            val t = Tournament(Date(), Date(), mutableListOf(""), true)
+                            t.name = name
+                            tournaments.add(t)
+                            navController.popBackStack()
                         }) {
                             Icon(Icons.Default.Check, "Save and exit")
                         }
@@ -72,8 +68,8 @@ fun TournamentEditor(navController: NavController, tournaments: MutableList<Tour
                 ) {
                     item {
                         TextField(
-                            value = name.value,
-                            onValueChange = { name.value = it },
+                            value = name,
+                            onValueChange = { name = it },
                             singleLine = true,
                             label = { Text("Name") },
                             placeholder = { Text(text = "Give it a meaningful name") },
@@ -83,36 +79,46 @@ fun TournamentEditor(navController: NavController, tournaments: MutableList<Tour
                     }
                     item {
                         val context = LocalContext.current
-                        TextButton(onClick = {
-                            DatePickerDialog(
-                                context, R.style.Theme_TournamentsCompose_Dialog,
-                                { _, year, month, day -> date.value = "$year/${month + 1}/$day" },
-                                2022,
-                                7,
-                                11
-                            ).show()
-                        }) {
-                            Text(text = "Start Date: ${date.value}")
+                        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                            ElevatedButton(
+                                onClick = {
+                                    DatePickerDialog(
+                                        context, R.style.Theme_TournamentsCompose_Dialog,
+                                        { _, y, m, d -> start = GregorianCalendar(y, m, d) },
+                                        start.get(Calendar.YEAR),
+                                        start.get(Calendar.MONTH),
+                                        start.get(Calendar.DAY_OF_MONTH)
+                                    ).show()
+                                }, modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    text = "Start Date: ${start.get(Calendar.YEAR)}" +
+                                            "/${start.get(Calendar.MONTH) + 1}" +
+                                            "/${start.get(Calendar.DAY_OF_MONTH)}"
+                                )
+                            }
+                            ElevatedButton(
+                                onClick = {
+                                    DatePickerDialog(
+                                        context, R.style.Theme_TournamentsCompose_Dialog,
+                                        { _, y, m, d -> end = GregorianCalendar(y, m, d) },
+                                        end.get(Calendar.YEAR),
+                                        end.get(Calendar.MONTH),
+                                        end.get(Calendar.DAY_OF_MONTH)
+                                    ).show()
+                                }, modifier = Modifier
+                                    .fillMaxWidth()
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    text = "End Date: ${end.get(Calendar.YEAR)}" +
+                                            "/${end.get(Calendar.MONTH) + 1}" +
+                                            "/${end.get(Calendar.DAY_OF_MONTH)}"
+                                )
+                            }
                         }
-                    }
-                    item {
-                        Text(text = "Edit here")
-                    }
-                    item {
-                        ElevatedButton(
-                            onClick = {
-                                val t = Tournament(Date(), Date(), mutableListOf(""), true)
-                                t.name = name.value
-                                tournaments.add(t)
-                                navController.popBackStack()
-                            },
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(text = "Back")
-                        }
-                    }
-                    items(50) {
-                        Text(text = "Edit here")
                     }
                 }
                 SnackbarHost(
