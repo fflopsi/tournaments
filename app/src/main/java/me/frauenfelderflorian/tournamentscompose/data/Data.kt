955c2256b1e038a59ea2deb20d895a737a96c478
fflopsi <florian.l.frauenfelder@gmail.com>
2023-03-21 15:00:39 +0100

    Move TournamentContainer, Tournament and Game classes into one single file

--- app/src/main/java/me/frauenfelderflorian/tournamentscompose/data/Data.kt
+++ app/src/main/java/me/frauenfelderflorian/tournamentscompose/data/Data.kt
@@ -1,5 +1,18 @@
 package me.frauenfelderflorian.tournamentscompose.data
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class TournamentContainer : ViewModel() {
+    var current = -1; private set
+    val tournaments = mutableStateListOf<Tournament>()
+
+    fun updateCurrent(new: Int) {
+        if (new < -1 || new >= tournaments.size) throw ArrayIndexOutOfBoundsException()
+        current = new
+    }
+}
+
 class Tournament(
     var start: Long,
     var end: Long,
@@ -9,12 +22,10 @@ class Tournament(
 ) {
     var current = -1
     val games = mutableListOf<Game>()
-//    val currentGame get() = games[current]
     var name = ""
         set(value) {
             field = value.trim()
         }
-    val gamesByDate get() = games.sortedByDescending { it.date }
     val playersByPoints get() = players.sortedByDescending { getPoints(it) }
 
     fun updateCurrent(new: Int) {
@@ -37,10 +48,25 @@ class Tournament(
                 else if (game.ranking[player] == 1) points += firstPoints
                 else if (game.ranking[player] == 2) points += firstPoints - 3
                 else if (game.ranking[player] == 3) points += firstPoints - 5
-                else if (game.ranking[player]!! > 3)
-                    points += firstPoints - (game.ranking[player]!! + 3)
+                else if (game.ranking[player]!! > 3) points += firstPoints - (game.ranking[player]!! + 3)
             }
         }
         return points
     }
+}
+
+class Game(var date: Long, var hoops: Int, var hoopReached: Int) {
+    var difficulty = "not set"
+        set(value) {
+            field = value.trim()
+        }
+    val ranking = mutableMapOf<String, Int>()
+    val absentPlayers get() = ranking.filterValues { it == 0 }.keys
+    val playersByRank: List<String>
+        get() {
+            val players = mutableListOf<String>()
+            for (i in 0 until ranking.size - absentPlayers.size)
+                players.add(ranking.filterValues { it == i + 1 }.keys.first())
+            return players.toList()
+        }
 }
