86e3c6ca8667f5704ec55b534de27f0c2dabd598
fflopsi <florian.l.frauenfelder@gmail.com>
2023-03-21 16:44:36 +0100

    Change BottomNavigation to Tabs

--- app/src/main/java/me/frauenfelderflorian/tournamentscompose/TournamentViewer.kt
+++ app/src/main/java/me/frauenfelderflorian/tournamentscompose/TournamentViewer.kt
@@ -6,9 +6,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.EmojiEvents
-import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,9 +36,9 @@ fun TournamentViewer(
 ) {
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    var selectedTab by rememberSaveable { mutableStateOf(0) }
 
     TournamentsComposeTheme(darkTheme = getTheme(theme = theme)) {
-        var selectedPage by rememberSaveable { mutableStateOf(0) }
         Scaffold(
             topBar = {
                 LargeTopAppBar(
@@ -49,7 +50,7 @@ fun TournamentViewer(
                             Icon(Icons.Default.ArrowBack, stringResource(R.string.back))
                         }
                     },
-                    actions = { //TODO: fix
+                    actions = {
                         IconButton(onClick = { navController.navigate(Routes.TOURNAMENT_EDITOR.route) }) {
                             Icon(Icons.Default.Edit, stringResource(R.string.edit_tournament))
                         }
@@ -59,7 +60,7 @@ fun TournamentViewer(
             },
             floatingActionButton = {
                 AnimatedVisibility(
-                    visible = selectedPage == 0,
+                    visible = selectedTab == 0,
                     enter = scaleIn(),
                     exit = scaleOut()
                 ) {
@@ -74,94 +75,98 @@ fun TournamentViewer(
                     )
                 }
             },
-            bottomBar = {
-                NavigationBar { //TODO: change to tabs
-                    NavigationBarItem(
-                        selected = selectedPage == 0,
-                        icon = {
-                            if (selectedPage == 0) Icon(Icons.Default.EmojiEvents, null)
-                            else Icon(Icons.Outlined.EmojiEvents, null)
-                        },
-                        label = { Text(stringResource(R.string.games)) },
-                        onClick = { selectedPage = 0 }
-                    )
-                    NavigationBarItem(
-                        selected = selectedPage == 1,
-                        icon = {
-                            if (selectedPage == 1) Icon(Icons.Default.Person, null)
-                            else Icon(Icons.Outlined.Person, null)
-                        },
-                        label = { Text(stringResource(R.string.leaderboard)) },
-                        onClick = { selectedPage = 1 }
-                    )
-                }
-            },
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
         ) { paddingValues ->
-            AnimatedVisibility(
-                visible = selectedPage == 0,
-                enter = slideInHorizontally(initialOffsetX = { width -> -width }),
-                exit = slideOutHorizontally(targetOffsetX = { width -> -width })
+            Column(
+                modifier = Modifier.padding(paddingValues),
+                verticalArrangement = Arrangement.spacedBy(0.dp)
             ) {
-                LazyColumn(
-                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier.padding(paddingValues)
-                ) {
-                    if (tournament.games.isNotEmpty())
-                        items(items = tournament.games.sortedByDescending { it.date }) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.clickable {
-                                    tournament.updateCurrent(tournament.games.indexOf(it))
-                                    navController.navigate(Routes.GAME_VIEWER.route)
+                TabRow(selectedTabIndex = selectedTab) {
+                    Tab(
+                        selected = selectedTab == 0,
+                        onClick = { selectedTab = 0 },
+                        text = { Text(stringResource(R.string.details)) }
+                    )
+                    Tab(
+                        selected = selectedTab == 1,
+                        onClick = { selectedTab = 1 },
+                        text = { Text(stringResource(R.string.ranking)) }
+                    )
+                }
+                val columnScope = this
+                Box {
+                    columnScope.AnimatedVisibility(
+                        visible = selectedTab == 0,
+                        enter = slideInHorizontally(initialOffsetX = { width -> -width }),
+                        exit = slideOutHorizontally(targetOffsetX = { width -> -width })
+                    ) {
+                        LazyColumn(
+                            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            if (tournament.games.isNotEmpty())
+                                items(items = tournament.games.sortedByDescending { it.date }) {
+                                    Row(
+                                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        modifier = Modifier.clickable {
+                                            tournament.updateCurrent(tournament.games.indexOf(it))
+                                            navController.navigate(Routes.GAME_VIEWER.route)
+                                        }
+                                    ) {
+                                        Text(
+                                            text = stringResource(
+                                                R.string.game_title,
+                                                formatDate(it.date)
+                                            ),
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .weight(2f)
+                                        )
+                                        IconButton(onClick = {
+                                            tournament.updateCurrent(tournament.games.indexOf(it))
+                                            navController.navigate(Routes.GAME_EDITOR.route)
+                                        }) {
+                                            Icon(
+                                                Icons.Default.Edit,
+                                                stringResource(R.string.edit_game)
+                                            )
+                                        }
+                                    }
                                 }
-                            ) {
+                            else item {
                                 Text(
-                                    text = stringResource(R.string.game_title, formatDate(it.date)),
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .weight(2f)
+                                    text = stringResource(R.string.add_first_game_hint),
+                                    fontStyle = FontStyle.Italic,
+                                    fontWeight = FontWeight.ExtraLight
                                 )
-                                Spacer(modifier = Modifier.width(16.dp))
-                                IconButton(onClick = {
-                                    tournament.updateCurrent(tournament.games.indexOf(it))
-                                    navController.navigate(Routes.GAME_EDITOR.route)
-                                }) {
-                                    Icon(Icons.Default.Edit, stringResource(R.string.edit_game))
-                                }
                             }
                         }
-                    else item {
-                        Text(
-                            text = stringResource(R.string.add_first_game_hint),
-                            fontStyle = FontStyle.Italic,
-                            fontWeight = FontWeight.ExtraLight
-                        )
                     }
-                }
-            }
-            AnimatedVisibility(
-                visible = selectedPage == 1,
-                enter = slideInHorizontally(initialOffsetX = { width -> width }),
-                exit = slideOutHorizontally(targetOffsetX = { width -> width })
-            ) {
-                LazyColumn(
-                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier.padding(paddingValues)
-                ) {
-                    items(items = tournament.playersByPoints) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text(
-                                text = it + ": " + tournament.getPoints(it),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .weight(2f)
-                            )
-                            Spacer(modifier = Modifier.width(16.dp))
-                            IconButton(onClick = { /*TODO*/ }) {
-                                Icon(Icons.Default.MoreHoriz, null)
+                    columnScope.AnimatedVisibility(
+                        visible = selectedTab == 1,
+                        enter = slideInHorizontally(initialOffsetX = { width -> width }),
+                        exit = slideOutHorizontally(targetOffsetX = { width -> width })
+                    ) {
+                        LazyColumn(
+                            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+                            verticalArrangement = Arrangement.spacedBy(16.dp)
+                        ) {
+                            items(items = tournament.playersByPoints) {
+                                Row(
+                                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        text = it + ": " + tournament.getPoints(it),
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .weight(2f)
+                                    )
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(Icons.Default.MoreHoriz, null)
+                                    }
+                                }
                             }
                         }
                     }
