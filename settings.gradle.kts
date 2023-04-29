fad737527d01b46e8e9d6f0e90e37d9d96881a4f
fflopsi <florian.l.frauenfelder@gmail.com>
2023-04-29 09:51:27 +0200

    Add dependencies for Compose Multiplatform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,22 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        val kotlinVersion = "1.8.20"
+        val agpVersion = "8.0.0"
+        val composeVersion = "1.4.0"
+
+        kotlin("jvm").version(kotlinVersion)
+        kotlin("multiplatform").version(kotlinVersion)
+        kotlin("android").version(kotlinVersion)
+
+        id("com.android.application").version(agpVersion)
+        id("com.android.library").version(agpVersion)
+
+        id("org.jetbrains.compose").version(composeVersion)
     }
 }
 dependencyResolutionManagement {
@@ -10,6 +26,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "TournamentsCompose"
