409174d2c6d952c74148041c1773f5e454ab7e42
fflopsi <florian.l.frauenfelder@gmail.com>
2023-04-29 10:03:09 +0200

    Update version to 1.1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.frauenfelderflorian.tournamentscompose"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
