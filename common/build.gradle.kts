e16e2fbef8631c1d1e9c179addbe3667ec49a885
fflopsi <florian.l.frauenfelder@gmail.com>
2023-06-09 12:55:46 +0200

    Update dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -42,7 +42,7 @@ kotlin {
                 implementation("androidx.datastore:datastore-preferences:1.0.0")
                 implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
                 implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-                implementation("androidx.navigation:navigation-compose:2.5.3")
+                implementation("androidx.navigation:navigation-compose:2.6.0")
 
                 val roomVersion = "2.5.1"
                 implementation("androidx.room:room-runtime:$roomVersion")
